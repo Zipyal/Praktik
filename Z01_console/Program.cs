aab41102226cb66409374c2d2e02dbe4a3a3eb30
Zipyal <80532767+Zipyal@users.noreply.github.com>
2021-12-01 23:57:58 +0300

    Add files via upload

---
+++ Z01_console/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    [DebuggerDisplay($"{{{nameof(GetDebuggerDisplay)}(),nq}}")]
+    class Program : ProgramBase1
+    {
+private string GetDebuggerDisplay()
+        {
+            double x, y;
+            Console.WriteLine("Введите x");
+            x = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите y");
+            y = double.Parse(Console.ReadLine());
+            if ((y < x) && (((x * x) + (y * y)) < 144))
+            {
+                Console.WriteLine("Точка попала в заштрихованную область");
+            }
+            else if ((y = x) && (((x * x) + (y * y)) == 144))
+            {
+                Console.WriteLine("Точка на границе заштрихованной области");
+            }
+            else
+            {
+                Console.WriteLine("Точка не попала в заштрихованную область");
+            }
+            Console.ReadLine();
+        }
+    }
---
+++ Z01_form/Form2.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string str = textBox1.Text;
+            int a = int.Parse(str);
+
+            NewMethod(a);
+
+        }
+
+        private void NewMethod(int a)
+        {
+            NewMethod1(a);
+        }
+
+        private void NewMethod1(int a)
+        {
+            if (a <= 0x0 || a >= 1000)
+                MessageBox.Show("Некоректное число. Введите целое число!");
+            else
+            {
+                // нахождение числа
+                textBox2.Text = ((a % 10) + (a / 10 % 10)) % 2 == 0 ? "Число - является четным" : "Число - не является четным";
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e) => Close();
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
