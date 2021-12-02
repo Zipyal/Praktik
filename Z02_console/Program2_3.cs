62f2d307595651357d355ef7f6657ff6d16b3674
Zipyal <80532767+Zipyal@users.noreply.github.com>
2021-12-02 22:06:27 +0300

    Add files via upload

---
+++ Z02_console/Program2_3.cs
@@ -0,0 +1,20 @@
+﻿//цикл FOR
+for (int i = 60; i >= 10; i -= 2)
+{
+    Console.WriteLine(i);
+}
+//цикл WHILE
+int k = 60;
+while (k >= 10)
+{
+    Console.WriteLine(k);
+    k -= 2;
+}
+//цикл DO WHILE
+int n = 60;
+do
+{
+    Console.WriteLine(n);
+    n -= 2;
+}
+while (n >= 10);
