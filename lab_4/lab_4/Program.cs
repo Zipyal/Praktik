a7f040024484f6cec0e882303b00d6392c9fb4b2
Zipyal <80532767+Zipyal@users.noreply.github.com>
2021-12-22 08:45:15 +0300

    Add files via upload

---
+++ lab_4/lab_4/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ConsoleApplication2
+{
+    class Program
+    {
+        private int f(int n)
+        {
+            if (n == 1) return -10; if (n == 2) return 2;
+            return Math.Abs(f(n - 2)) - 6 * f(n - 1);
+        }
+
+        static void Main(string[] args)
+        {
+            double q = double.Parse(Console.ReadLine());
+            Console.Write("n = ");
+            int n = int.Parse(Console.ReadLine());
+            f(n);
+            Console.Write("b{0} = {1}", n);
+
+            Console.ReadLine();
+
+        }
+
+    }
+}
+}
+}
---
+++ lab_4_1_c/lab_4_1_c/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ConsoleApplication2
+{
+    class Program
+    {
+        private int f(int n)
+        {
+            if (n == 1) return -10; if (n == 2) return 2;
+            return Math.Abs(f(n - 2)) - 6 * f(n - 1);
+        }
+
+        static void Main(string[] args)
+        {
+            double q = double.Parse(Console.ReadLine());
+            Console.Write("n = ");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.Write("f(n);", n);
+
+            Console.ReadLine();
+
+        }
+
+    }
+}
