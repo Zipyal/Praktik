b04dba785c1a332e43ce2ed65de9754a5ffd9825
Zipyal <80532767+Zipyal@users.noreply.github.com>
2021-12-22 08:50:38 +0300

    Add files via upload

---
+++ lab_9_1/lab_9_1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+const string FILE_PATH = "file.txt";
+const int COUNT = 5;
+
+var numbers = Enumerable
+    .Range(0, COUNT)
+    .Select(exponent => Math.Pow(3, exponent));
+
+await File.WriteAllTextAsync(FILE_PATH, string.Join(Environment.NewLine, numbers));
+
+var fileNumbers = await File.ReadAllLinesAsync(FILE_PATH);
+for (var i = 0; i < fileNumbers.Length; i += 2)
+{
+    Console.WriteLine(fileNumbers[i]);
+}
---
+++ lab_9_2_c/lab_9_2_c/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите заданную букву: ");
+        char a = char.Parse(Console.ReadLine());
+        int k = 0;
+        foreach (String s in File.ReadAllLines("text.txt"))
+        {
+            string s1 = Regex.Replace(s, "[-.?!)(,:]", "");
+
+            s1 = s1.ToLower().Trim();
+
+            if (s1 != null && s1.Length > 0 && s1[0] == a && s1[s1.Length - 1] == a)
+                k++;
+        }
+
+        Console.WriteLine("k= " + k);
+        Console.ReadKey();
+    }
+}
