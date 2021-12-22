de02a17b2af688ae6430ea1aa127d3b936afb343
Zipyal <80532767+Zipyal@users.noreply.github.com>
2021-12-22 08:46:02 +0300

    Add files via upload

---
+++ lab_6_1_1_c/lab_6_1_1_c/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите размер массива:");
+            var arraySize = Convert.ToInt32(Console.ReadLine());
+            var array = new int[arraySize];
+
+            for (var i = 0; i < array.Length; i++)
+            {
+                Console.Write("a[{0}]= ", i);
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("Введите число:");
+            var number = Convert.ToInt32(Console.ReadLine());
+
+            var result = Array.ConvertAll(array, e => (e < number) ? number : e);
+
+            Console.Write("Результат: ");
+            foreach (var t in result)
+            {
+                Console.Write(t + " ");
+            }
+
+            Console.ReadLine();
+        }
+    }
