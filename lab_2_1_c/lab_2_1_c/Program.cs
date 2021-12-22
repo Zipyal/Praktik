bed0dc5959e68983673280960e32f24ee463931d
Zipyal <80532767+Zipyal@users.noreply.github.com>
2021-12-22 08:43:22 +0300

    Add files via upload

---
+++ lab_2_1_c/lab_2_1_c/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("x=");
+        float x = float.Parse(Console.ReadLine()); Console.Write("y=");
+        float y = float.Parse(Console.ReadLine());
+        if (x * x + y * y < 81 && x > 0) Console.WriteLine("внутри"); else if (x * x + y * y > 81 || x < 0) Console.WriteLine("вне"); else Console.WriteLine("на границе");
+    }
+}
