ec18c71a8fea1e4f275280927b1e129bd804aa59
Zipyal <80532767+Zipyal@users.noreply.github.com>
2021-12-22 08:48:46 +0300

    Add files via upload

---
+++ lab_6_3_c/lab_6_3_c/Program.cs
@@ -0,0 +1,31 @@
+﻿class program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Какой размер массива : ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        int[,] myArray_2 = new int[n, n];
+        Random rnd = new Random();
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine();
+            for (int j = 0; j < n; j++)
+            {
+                myArray_2[i, j] = rnd.Next(10);
+                Console.Write("{0} ", myArray_2[i, j]);
+            }
+        }
+        int Average = 0, del = 1;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j <= i; j++)
+                if (myArray_2[i, j] % 2 == 0)
+                {
+                    Average += myArray_2[i, j];
+                    del++;
+                }
+        }
+        Console.WriteLine("\nОтвет: {0}", Average / del);
+        Console.ReadKey();
+    }
+}
---
+++ lab_6_4_c/lab_6_4_c/Program.cs
@@ -0,0 +1,38 @@
+﻿class program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Сколько ступенек будет в массиве : ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        int[][] myArray_1 = new int[n][];
+        int[] minArray = new int[n];
+        Random rnd = new Random();
+
+        for (int i = 0; i < myArray_1.Length; i++)
+        {
+            Console.WriteLine();
+            myArray_1[i] = new int[i + 1];
+            for (int j = 0; j <= i; j++)
+            {
+                myArray_1[i][j] = rnd.Next(10);
+                Console.Write(myArray_1[i][j] + " ");
+            }
+        }
+
+        int min = 0;
+        for (int i = 0; i < myArray_1.Length; i++)
+        {
+            min = myArray_1[i][0];
+            for (int j = 0; j <= i; j++)
+            {
+                if (min >= myArray_1[i][j])
+                    min = myArray_1[i][j];
+            }
+            minArray[i] = min;
+        }
+        Console.WriteLine("\n~~ Минимальные Элементы ~~");
+        foreach (int value in minArray)
+            Console.Write("{0} ", value);
+        Console.ReadKey();
+    }
+}
