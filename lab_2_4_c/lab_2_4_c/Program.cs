f848ff0ed361064e28e2f445e0370d28a73f7fd8
Zipyal <80532767+Zipyal@users.noreply.github.com>
2021-12-22 08:44:25 +0300

    Add files via upload

---
+++ lab_2_4_c/lab_2_4_c/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.IO;
+using System;
+using System.Linq;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("{0}", String.Join(" "
+, Enumerable.Range(1, 4).Select(v => String.Join(" ", Enumerable.Range(1, 10)) + '\n')));
+    }
+}
